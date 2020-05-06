3282091a2032bc4c082869874ef013797c772545
Kenold Beauplan <something4ken@gmail.com>
2020-05-06 16:12:22 -0400

    check if document is loaded

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-// TODO: add ./img in DOM
 const titles = [{
         id: 1,
         name: "The Half of it",
@@ -36,121 +35,122 @@ const titles = [{
     }
 ];
 
-const myList = document.querySelector("#my-list");
-const addToListButton = document.querySelector("#btn-add");
+document.addEventListener('DOMContentLoaded', () => {
 
-// get DOM elements
-const titleLogo = document.querySelector(".title-logo-img");
-const titleName = titleLogo.title;
-const titleBg = document.querySelector(".screen-container");
-const titleStatus = document.querySelector(".status");
-const titleYear = document.querySelector(".year");
-const titleMpaa = document.querySelector(".mpaa");
-const titleDuration = document.querySelector(".duration");
-const titlePopularity = document.querySelector(".popularity-number");
-const titleSynopsis = document.querySelector(".synopsis");
-const titleCast = document.querySelector(".cast");
-const titleCrew = document.querySelector(".crew");
-// crewHeading: Director or Creator
-const titleCrewHeading = document.querySelector('.crew-info .heading')
-const titleGenres = document.querySelector(".genres");
+    const myList = document.querySelector("#my-list");
+    const addToListButton = document.querySelector("#btn-add");
 
-
-// get a random index from the titles array
-const randIndex = Math.floor(Math.random() * Math.floor(titles.length));
-
-// save the chosen title in the title variable
-const title = titles[randIndex];
-
-// *** SET DOM VALUES ***
-
-// .screen-container div
-titleBg.style.background = `url(./img/${title.background}) no-repeat right center`;
-titleBg.style.backgroundSize = "cover";
-
-// logo image
-titleLogo.src = `./img/${title.logo}`;
-titleLogo.alt = title.name;
-
-// meta
-titleStatus.innerText = title.status;
-titleYear.innerText = title.year;
-titleMpaa.innerText = title.mpaa;
-
-const duration = title.type === "Movie" ? title.duration : (title.seasons + " Seasons");
-titleDuration.innerText = duration;
-
-// popularity
-titlePopularity.innerText = title.popularity;
-
-// synopsis
-titleSynopsis.innerText = title.synopsis;
-
-// cast and crew heading
-/*
-    if the title is a movie, set the crew heading to "Director"
-    else set the heading to "Creator"
-    pulralize() add the 's' suffix to each headings
-*/
-const heading = title.type === "Movie" ?
-    pluralize("Director", title.crew) :
-    pluralize("Creator", title.crew);
-titleCrewHeading.innerText = heading;
-
-// cast and crew names
-titleCast.innerText = title.cast.join(", ");
-titleCrew.innerText = title.crew.join(", ");
-
-// genres
-titleGenres.innerText = title.genres;
-
-//  *** EVENT HANDLERS ***
-addToListButton.addEventListener("click", function (e) {
-    addToList(title);
-});
-
-myList.addEventListener("click", displayList);
+    // get DOM elements
+    const titleLogo = document.querySelector(".title-logo-img");
+    const titleName = titleLogo.title;
+    const titleBg = document.querySelector(".screen-container");
+    const titleStatus = document.querySelector(".status");
+    const titleYear = document.querySelector(".year");
+    const titleMpaa = document.querySelector(".mpaa");
+    const titleDuration = document.querySelector(".duration");
+    const titlePopularity = document.querySelector(".popularity-number");
+    const titleSynopsis = document.querySelector(".synopsis");
+    const titleCast = document.querySelector(".cast");
+    const titleCrew = document.querySelector(".crew");
+    // crewHeading: Director or Creator
+    const titleCrewHeading = document.querySelector('.crew-info .heading')
+    const titleGenres = document.querySelector(".genres");
 
 
-// *** FUNCTIONS ***
+    // get a random index from the titles array
+    const randIndex = Math.floor(Math.random() * Math.floor(titles.length));
 
-/*
-    pluralize() addS the 's' suffix
-    to "Director" and "Creator" headings
-    if the peopleArray is > 1
-    ex. call: pluralize("Creator", title.crew)
-    - title.crew is an array
-*/
-function pluralize(peopleHeading, peopleArray) {
-    if (peopleArray.length > 1) {
-        return peopleHeading + "s: ";
-    } else {
-        return peopleHeading + ": ";
+    // save the chosen title in the title variable
+    const title = titles[randIndex];
+
+    // *** SET DOM VALUES ***
+
+    // .screen-container div
+    titleBg.style.background = `url(./img/${title.background}) no-repeat right center`;
+    titleBg.style.backgroundSize = "cover";
+
+    // logo image
+    titleLogo.src = `./img/${title.logo}`;
+    titleLogo.alt = title.name;
+
+    // meta
+    titleStatus.innerText = title.status;
+    titleYear.innerText = title.year;
+    titleMpaa.innerText = title.mpaa;
+
+    const duration = title.type === "Movie" ? title.duration : (title.seasons + " Seasons");
+    titleDuration.innerText = duration;
+
+    // popularity
+    titlePopularity.innerText = title.popularity;
+
+    // synopsis
+    titleSynopsis.innerText = title.synopsis;
+
+    // cast and crew heading
+    /*
+        if the title is a movie, set the crew heading to "Director"
+        else set the heading to "Creator"
+        pulralize() add the 's' suffix to each headings
+    */
+    const heading = title.type === "Movie" ?
+        pluralize("Director", title.crew) :
+        pluralize("Creator", title.crew);
+    titleCrewHeading.innerText = heading;
+
+    // cast and crew names
+    titleCast.innerText = title.cast.join(", ");
+    titleCrew.innerText = title.crew.join(", ");
+
+    // genres
+    titleGenres.innerText = title.genres;
+
+    //  *** EVENT HANDLERS ***
+    addToListButton.addEventListener("click", function (e) {
+        addToList(title);
+    });
+
+    myList.addEventListener("click", displayList);
+
+
+    // *** FUNCTIONS ***
+    /*
+        pluralize() addS the 's' suffix
+        to "Director" and "Creator" headings
+        if the peopleArray is > 1
+        ex. call: pluralize("Creator", title.crew)
+        - title.crew is an array
+    */
+    function pluralize(peopleHeading, peopleArray) {
+        if (peopleArray.length > 1) {
+            return peopleHeading + "s: ";
+        } else {
+            return peopleHeading + ": ";
+        }
     }
-}
 
-function addToList(title) {
-    // check if localstorage has data
-    let myList;
-    if (localStorage.getItem("myList") === null) {
-        myList = [];
-    } else {
-        myList = JSON.parse(localStorage.getItem("myList"))
+    function addToList(title) {
+        // check if localstorage has data
+        let myList;
+        if (localStorage.getItem("myList") === null) {
+            myList = [];
+        } else {
+            myList = JSON.parse(localStorage.getItem("myList"))
+        }
+        // push title object to myList array
+        myList.push(title);
+
+        // add list to local storage
+        localStorage.setItem("myList", JSON.stringify(myList));
+        console.log(`"${title.name}" was added to your list`);
     }
-    // push title object to myList array
-    myList.push(title);
 
-    // add list to local storage
-    localStorage.setItem("myList", JSON.stringify(myList));
-    console.log(`"${title.name}" was added to your list`);
-}
+    function displayList() {
+        console.log("MY LIST");
 
-function displayList() {
-    console.log("MY LIST");
-
-    const allTitles = JSON.parse(localStorage.getItem("myList"));
-    allTitles.forEach(function (title) {
-        console.log("- " + title.name);
-    })
-
-}
+        const allTitles = JSON.parse(localStorage.getItem("myList"));
+        allTitles.forEach(function (title) {
+            console.log("- " + title.name);
+        })
+    }
+})
