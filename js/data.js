9178574253efbaf9fe0451e6e8336abc8c02b7b9
Kenold Beauplan <something4ken@gmail.com>
2020-05-10 12:49:49 -0400

    initial commit

---
+++ js/data.js
@@ -0,0 +1,93 @@
+export const titles = [{
+        "id": 1,
+        "name": "The Half Of It",
+        "year": 2020,
+        "type": "Movie",
+        "mpaa": "PG-13",
+        "duration": "1h 45m",
+        "popularity": 9,
+        "synopsis": "When smart but cash-strapped teen Ellie Chu agrees to write a love letter for a jock, she doesn't expect to become his friend — or fall for his crush.",
+        "cast": [
+            "Leah Lewis",
+            "Daniel Diemer"
+        ],
+        "crew": [
+            "Alice Wu"
+        ],
+        "genres": "LGBTQ Movies, Teen Movies",
+        "status": "New",
+        "poster": "the-half-of-it-poster.jpg",
+        "background": "the-half-of-it-bg.jpg",
+        "logo": "the-half-of-it-logo.png"
+    },
+    {
+        "id": 2,
+        "name": "Ozark",
+        "year": 2017,
+        "type": "Series",
+        "seasons": 3,
+        "mpaa": "TV-MA",
+        "popularity": 7,
+        "synopsis": "A financial adviser drags his family from Chicago to the Missouri Ozarks, where he must launder $500 million in five years to appease a drug boss.",
+        "cast": [
+            "Jason Bateman",
+            "Laura Linney",
+            "Sofia Hublitz"
+        ],
+        "crew": [
+            "Bill Dubuque",
+            "Mark Williams"
+        ],
+        "genres": "TV Dramas",
+        "status": "98% Match",
+        "poster": "ozark-poster.jpg",
+        "background": "ozark-bg.png",
+        "logo": "ozark-logo.png"
+    },
+    {
+        "id": 3,
+        "name": "Extraction",
+        "year": 2020,
+        "type": "Movie",
+        "mpaa": "R",
+        "duration": "1h 20m",
+        "popularity": 1,
+        "synopsis": "A hardened mercenary's mission becomes a soul-searching race to survive when he's sent into Bangladesh to rescue a drug lord's kidnapped son.",
+        "cast": [
+            "Chris Hemsworth",
+            "Rudhraksh Jaiswal",
+            "Randeep Hooda"
+        ],
+        "crew": [
+            "Sam Hargrave"
+        ],
+        "genres": "Action, Thrillers",
+        "status": "New",
+        "poster": "extraction-poster.jpg",
+        "background": "extraction-bg.jpg",
+        "logo": "extraction-logo.png"
+    },
+    {
+        "id": 4,
+        "name": "Hotel Transylvania 3: Summer Vacation",
+        "year": 2018,
+        "type": "Movie",
+        "mpaa": "PG",
+        "duration": "1h 37m",
+        "popularity": 2,
+        "synopsis": "It's love at first sight for Dracula when he meets Ericka, the charming but mysterious captain of the monster cruise that Mavis plans for the family.",
+        "cast": [
+            "Adam Sandler",
+            "Selena Gomez",
+            "Kevin James"
+        ],
+        "crew": [
+            "Genndy Tartakovsky"
+        ],
+        "genres": "Comedies",
+        "status": "",
+        "poster": "hotel-transylvania-3-poster.jpg",
+        "background": "hotel-transylvania-3-bg.jpg",
+        "logo": "hotel-transylvania-3-logo.png"
+    },
+]
