654cdad3bab2916185451e7d809e686c933140da
Kenold Beauplan <something4ken@gmail.com>
2020-05-13 18:52:52 -0400

    reformat code

--- js/list.js
+++ js/list.js
@@ -1,45 +1,48 @@
 function add(title) {
-    let myList;
+  let myList;
 
-    // check if there is data in local storage
-    if (localStorage.getItem("myList") === null) {
-        // if not, set an empty array
-        myList = [];
-    } else {
-        // if there is, pull it out and add to it
-        myList = JSON.parse(localStorage.getItem("myList"))
-    }
+  // check if there is data in local storage
+  if (localStorage.getItem("myList") === null) {
+    // if not, set an empty array
+    myList = [];
+  } else {
+    // if there is, pull it out and add to it
+    myList = JSON.parse(localStorage.getItem("myList"));
+  }
 
-    // check for duplicates
-    if (myList.some(function (currentTitle) {
-            return currentTitle.id === title.id
-        })) {
-        console.log(`"${title.name}" is already in your list.`);
-    } else {
-        myList.push(title);
-        localStorage.setItem("myList", JSON.stringify(myList));
-        console.log(`"${title.name}" was added to your list`);
+  // check for duplicates
+  if (
+    myList.some(function (currentTitle) {
+      return currentTitle.id === title.id;
+    })
+  ) {
+    console.log(`"${title.name}" is already in your list.`);
+  } else {
+    myList.push(title);
+    localStorage.setItem("myList", JSON.stringify(myList));
+    console.log(`"${title.name}" was added to your list`);
 
-        // update button text
-        const btnAdd = document.querySelector("#btn-add .icon-text");
-        btnAdd.innerText = "Remove from My List";
+    // update button text
+    const btnAdd = document.querySelector("#btn-add");
+    const btnAddText = document.querySelector("#btn-add .icon-text");
+    btnAddText.innerText = "Remove from My List";
 
-        // hide the vertical bar from the + icon
-        const verticalBar = document.querySelector(".icon-add .bar-v");
-        verticalBar.classList.add("hide");
-    }
+    // hide the vertical bar from the + icon
+    const verticalBar = document.querySelector(".icon-add .bar-v");
+    verticalBar.classList.add("hide");
+
+    // update button ID
+    // btnAdd.id = "btn-remove";
+  }
 }
 
 function display() {
-    console.log("MY LIST");
+  console.log("MY LIST");
 
-    const allTitles = JSON.parse(localStorage.getItem("myList"));
-    allTitles.forEach(function (title) {
-        console.log("- " + title.name);
-    })
+  const allTitles = JSON.parse(localStorage.getItem("myList"));
+  allTitles.forEach(function (title) {
+    console.log("- " + title.name);
+  });
 }
 
-export {
-    add,
-    display
-};
+export { add, display };
